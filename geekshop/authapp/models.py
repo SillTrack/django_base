3c8bd85008572a5e82c5773582a9281dba6ab8f9
Sill_Track <manuxn2002@bk.ru>
2022-08-17 19:20:59 +0300

    lesson-8 homework

---
+++ geekshop/authapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class ShopUser(AbstractUser):
+    avatar = models.ImageField(upload_to='users_avatars', blank=True)
+
+    age = models.PositiveIntegerField(verbose_name='возраст')
+
+    is_active = models.BooleanField(verbose_name='активен', default=True)
+
